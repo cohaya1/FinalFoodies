b996bce37b2d1472b033912eb42aa3198409d98e
Chika Ohaya <chika.ohaya@gmail.com>
2022-02-12 21:21:30 -0500

    Initial Commit

---
+++ FinalFoodies/FinalFoodiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalFoodiesApp.swift
+//  FinalFoodies
+//
+//  Created by Chika Ohaya on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalFoodiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
